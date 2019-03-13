e2b2eb97cfc0fe63406014d99908eb60f2732274
Elin <elin.sexton@iths.se>
2019-03-13 15:04:43 +0100

    Added logger and logger comments to Controller

--- project2/src/main/java/com/example/project2/Controller.java
+++ project2/src/main/java/com/example/project2/Controller.java
@@ -1,5 +1,7 @@
 package com.example.project2;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
@@ -12,46 +14,43 @@ public class Controller {
 //    @Autowired
 //    private RestTemplate restTemplate;
 
-    private BookRepository repository;
+    Logger logger = LoggerFactory.getLogger(Controller.class);
 
-    List<Book> bookList = Arrays.asList(
-            new Book("Harry Potter", "Wizards, Witches and Kneezles"),
-            new Book("The Lion, the Witch and the Wardrobe", "Aslan"),
-            new Book("Matilda", "Little girl with a big brain")
-    );
+    private BookRepository repository;
 
     public Controller (BookRepository repository) {
         this.repository = repository;
 
     }
-//    @GetMapping("/books")
-//    public List<Book> getBookList(){
-//        return bookList;
-//    }
 
     @GetMapping("/books")
     public List<Book> getAll() {
+        logger.info("get all books in repository");
         return repository.findAll();
     }
 
     @GetMapping("/book/{bookId}")
     public Book getOne(@PathVariable Long bookId){
+        logger.info("get one book from repository");
         return repository.findById(bookId)
                 .orElseThrow( () -> new BooksException("No books with id:" + bookId));
     }
 
-    @PostMapping("/makebook")
+    @PostMapping("/addbook")
     public Book create(@RequestBody Book book) {
+       logger.info("add book to repository");
         return repository.save(book);
     }
 
     @DeleteMapping("/removebook{bookId}")
     public void delete(@PathVariable Long bookId){
+        logger.info("remove a book from repository");
         repository.deleteById(bookId);
     }
 
     @PutMapping("/updatebook{bookId}")
     public Book change(@RequestBody Book book, @PathVariable Long bookId){
+        logger.info("update book in repository");
         return repository.findById(bookId).map(storedBook -> {
             storedBook.setBookName(book.getBookName());
             storedBook.setDescription(book.getDescription());
